5d5c5725141c6eef5ee149bd9d537ba04d35111c
Camila Zucchi <camilazucchi@outlook.com>
2022-12-20 09:30:30 -0300

    fix: melhorando funções

--- assets/scripts/copiar.js
+++ assets/scripts/copiar.js
@@ -3,6 +3,6 @@ var saida = document.querySelector("p#saida");
 function copyToClipboard() {
     var copyText = saida.innerHTML;
     navigator.clipboard.writeText(copyText).then(() => {
-       saida.innerHTML = "Seu texto foi copiado com sucesso."
+       saida.innerHTML = "Seu texto foi copiado com sucesso. Divirta-se!"
     });
 }
--- assets/scripts/delete.js
+++ assets/scripts/delete.js
@@ -2,6 +2,10 @@ var texto = document.querySelector("textarea#main-input");
 var saida = document.querySelector("p#saida");
 
 function clearText() {
+  if (texto.value.length === 0) {
+    saida.innerHTML = "Digite alguma coisa antes de apagar!";
+  } else {
     texto.value = "";
-    saida.innerHTML = "Tente mais uma vez!";
+    saida.innerHTML = "";
+  }
 }
