e76bffcd1a92ff58068ccb32347c3754ad842094
Camila Zucchi <camilazucchi@outlook.com>
2022-12-26 20:06:36 -0300

    fix: alterando dom

--- assets/scripts/copiaTexto.js
+++ assets/scripts/copiaTexto.js
@@ -1,13 +1,13 @@
-var texto = document.querySelector("textarea#main__input");
-var saida = document.querySelector("p#main__output");
+var entradaTexto = document.querySelector("textarea#input__entrada");
+var saidaTexto = document.querySelector("textarea#input__saida");
 
 function copiaTexto() {
-    if (texto.value.length === 0) {
-        saida.innerHTML = "Ops, é preciso digitar algo para copiar!";
+    if (entradaTexto.value.length === 0) {
+        saidaTexto.innerHTML = "Ops, é preciso digitar algo para copiar!";
     } else {
-        var copyText = saida.innerHTML;
+        var copyText = saidaTexto.innerHTML;
         navigator.clipboard.writeText(copyText).then(() => {
-            saida.innerHTML = "Seu texto foi copiado com sucesso. Divirta-se!";
+            saidaTexto.innerHTML = "Seu texto foi copiado com sucesso. Divirta-se!";
         });
     }
 }
